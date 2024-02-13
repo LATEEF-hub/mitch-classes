ea21f9739c1d8fc13615231d69b4b6006f9087d5
LATEEF-hub <latifkk@krok.edu.ua>
2024-02-13 06:33:26 +0000

    Working with multiple IF statement- Flow control

--- new_app/lib/main.dart
+++ new_app/lib/main.dart
@@ -1,21 +1,76 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp() );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  String name = "Toby";
+  int age = 6;
+  double change = 15.56;
+  bool isMatured = false;
+
+  //Store Key- values pairs
+  Map user = {
+    'name': 'Toby Lateef',
+    'age': 33,
+    'height': 1.75,
+  };
+  //Ordered List
+  List<String> names = ["Ariana","Remilekun"];
+  //Stored unordered list
+  Set<String> uniqueNames = {"Moji", "Italy", "Pussy"};
+
+
+
+
+
+ 
+  /*
+    C O N T R O L F L O W
+    if ( condition ) {
+      do something;
+    }
+
+    if () {
+
+    } else {
+      do something
+    }
+  */
 
   @override
   Widget build(BuildContext context) {
-    print("What is your name?");
+    String grade = "F";
+
+    if ( grade == "A" ) {
+      print('Excellent');
+    } else if ( grade == "B" ) {
+      print('Great is your Good');
+    } else if ( grade == "C") {
+      print( 'Grade is Fair');
+    } else if ( grade == "D") {
+      print( 'Grade requires improvment' );
+    } else if ( grade == "E") {
+      print('Grade is Poor');
+    } else {
+      print( 'You are have failed woefully!!!');
+    }
+
+
+    switch (expression) {
+      case value:
+        
+        break;
+      default:
+    }
+
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+      ),
     );
   }
-
-
-
 }
