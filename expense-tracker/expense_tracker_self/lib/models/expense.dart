ad1abaa79b0a6ffac5374721f285fee296011d71
LATEEF-hub <latifkk@krok.edu.ua>
2024-02-14 02:14:46 +0000

    create stateful widget and install isar database for storing data

---
+++ expense-tracker/expense_tracker_self/lib/models/expense.dart
@@ -0,0 +1,22 @@
+import 'package:isar/isar.dart';
+
+
+//This line is needed to generate isar file for Database
+//Run cmd: dart run build_runner build
+part 'expense.g.dart';
+
+@Collection()
+class Expense {
+  Id id = Isar.autoIncrement; // 0,1,2,3...
+  final String name;
+  final double amount;
+  final  DateTime dateTime;
+  
+
+  Expense({
+    required this.name,
+    required this.amount,
+    required this.dateTime,
+  });
+}
+
